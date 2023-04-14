e3944a03ef6a350f9da8e4849d619033b3d27645
arwinzen <arwingoo90@gmail.com>
2023-04-14 18:31:18 +0800

    chap2.2.1

---
+++ chap2-hello/helloworld_internal_test.go
@@ -0,0 +1,19 @@
+package main
+
+import "testing"
+
+func ExampleMain() {
+    main()
+    // Output:
+    // Hello world
+}
+
+func TestGreet(t *testing.T) {
+    expectedGreeting := "Hello world"
+    greeting := greet()
+
+    if greeting != expectedGreeting {
+        // mark test as failed 
+        t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
+    }
+}
