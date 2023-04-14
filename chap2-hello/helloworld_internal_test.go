6538641f6766794f75fa3888df2cbd2709d4e17d
arwinzen <arwingoo90@gmail.com>
2023-04-14 23:42:04 +0800

    refactor: use map & table driven tests (2.3.1 - 2.3.2)

--- chap2-hello/helloworld_internal_test.go
+++ chap2-hello/helloworld_internal_test.go
@@ -2,36 +2,71 @@ package main
 
 import "testing"
 
+// tests output to stdout
+// by using Example<Function> Go will test stdout
+// against the next line which follows `// Output:`
 func ExampleMain() {
     main()
     // Output:
     // Hello world
 }
 
-func TestGreet_English(t *testing.T) {
-    expectedGreeting := "Hello world"
-    greeting := greet("en")
-
-    if greeting != expectedGreeting {
-        // mark test as failed 
-        t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
+func TestGreet(t *testing.T) {
+    // struct to define a test case
+    type testCase struct{
+        lang language
+        expectedGreeting string
     }
-}
 
-func TestGreet_French(t *testing.T) {
-    expectedGreeting := "Bonjour le monde"
-    greeting := greet("fr")
-    
-    if greeting != expectedGreeting {
-        t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
+    // map to store test cases
+    tests := map[string]testCase{
+        "English": {
+            // we can define the properties without keys
+            // since the order is defined as lang then greeting
+            "en",
+            "Hello world",
+        },
+        "French": {
+            // or we can be explicit by specifying the keys
+            lang:             "fr",
+            expectedGreeting: "Bonjour le monde",
+        },
+        "Akkadian, not supported": {
+            lang:             "akk",
+            expectedGreeting: `unsupported language: "akk"`,
+        },
+        "Greek": {
+            lang:             "el",
+            expectedGreeting: "Χαίρετε Κόσμε",
+        },
+        "Hebrew": {
+            lang:             "he",
+            expectedGreeting: "שלום עולם",
+        },
+        "Urdu": {
+            lang:             "ur",
+            expectedGreeting: "ہیلو دنیا",
+        },
+        "Vietnamese": {
+            lang:             "vi",
+            expectedGreeting: "Xin chào Thế Giới",
+        },
+        "Empty": {
+            lang:             "",
+            expectedGreeting: `unsupported language: ""`,
+        },
     }
-}
 
-func TestGreet_Akkadian(t *testing.T) {
-    expectedGreeting := ""
-    greeting := greet("ak")
-
-    if greeting != expectedGreeting {
-        t.Errorf("expected: %q, got: %q", expectedGreeting, greeting)
-    }
+    // range over all test cases
+    for name, tc := range tests {
+        // t.Run runs a closure on a subset of the tests
+        // and reports if the test is successful
+        t.Run(name, func(t *testing.T) {
+            greeting := greet(tc.lang)
+            
+            if greeting != tc.expectedGreeting {
+                t.Errorf("wanted: %q, got: %q", tc.expectedGreeting, greeting)
+            }
+        })
+    } 
 }
