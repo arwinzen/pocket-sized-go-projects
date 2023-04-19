6c02ad7e68d75de2172fd058c3cf06c3109bea7e
arwinzen <arwingoo90@gmail.com>
2023-04-19 09:12:36 +0800

    feat: initial pocketlogger setup

---
+++ pocketlog/logger.go
@@ -0,0 +1,22 @@
+package pocketlog
+
+type Logger struct {
+    threshold Level
+}
+
+func New(threshold Level) *Logger {
+    return &Logger{
+        threshold: threshold,
+    }
+}
+
+// Debugf formats and prints a message if the log level is debug or higher
+func (l *Logger) Debugf(format string, args ...any) {
+}
+
+// Info: formats and prints a message if the log level is info or higher
+func (l *Logger) Infof(format string, args ...any) {
+}
+
+func (l *Logger) Errorf(format string, args ...any) {
+}
