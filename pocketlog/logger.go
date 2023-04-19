09fe2bbc500b1ff42bb0f156a8007336baa4f934
arwinzen <arwingoo90@gmail.com>
2023-04-19 15:35:33 +0800

    feat: fill out method signatures for all loggers

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -14,11 +14,26 @@ func New(threshold Level) *Logger {
 
 // Debugf formats and prints a message if the log level is debug or higher
 func (l *Logger) Debugf(format string, args ...any) {
+    if l.threshold > LevelDebug {
+        return 
+    }
+
+    _, _ = fmt.Printf(format + "\n", args...)
 }
 
 // Info: formats and prints a message if the log level is info or higher
 func (l *Logger) Infof(format string, args ...any) {
+    if l.threshold > LevelInfo {
+        return 
+    }
+
+    _, _ = fmt.Printf(format + "\n", args...)
 }
 
 func (l *Logger) Errorf(format string, args ...any) {
+    if l.threshold > LevelError {
+        return 
+    }
+
+    _, _ = fmt.Printf(format + "\n", args...)
 }
